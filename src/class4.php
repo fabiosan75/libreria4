23a2ff22265a9273886d7ae00877882eb5804c53
fabiosan75 <fabiosan75@gmail.com>
2022-02-17 18:16:45 -0500

    crear libreria

---
+++ src/class4.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Class4 Class Doc Comment
+ *
+ * @category Class
+ * @package  Libreria4
+ * @author   fabiosan75 <fabiosan75@gmail.com>
+ * @license  http://www.gnu.org/copyleft/gpl.html GNU General Public License
+ * @link     https://github.com/fabiosan75/libreria4
+ *
+ */
+
+ namespace src;
+
+/**
+ * Class4
+ */
+
+class Class4
+{
+}
+
+
+?>
